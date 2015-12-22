b1b29c98f6552af2de5439f40744813e43f04e7a
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-22 23:52:44 +0530

    added more tests and strengthened article model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
            dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :articles, class_name: "Article"
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:rahul)
+    @article = Article.new(title: "This is my first post", content: "hellow how are you", user_id:@user.id)
+  end
+
+  test "this is valid" do
+    assert @article.valid?
+  end
+
+  test "user id and title should be present" do
+    @article.user_id = nil
+    assert_not @article.valid?
+    @article.title = nil
+    assert_not @article.valid?
+    @article.user_id = nil
+    assert_not @article.valid?
+    @article.content = nil
+    assert_not @article.valid?
+  end
+
+
 end
