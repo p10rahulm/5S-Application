61af38946ed3e1bf658668c7cc2d98abe2bfdbcf
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-23 16:08:42 +0530

    changed production config for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  host = 'frozen-escarpment-2842.herokuapp.com'
+  host = 'mysterious-sierra-9733.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.sendgrid.net',
